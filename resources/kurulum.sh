ae6ad1a60e9e5a84435fb9e5ab567c3deffe5847
Ismail <solist.cilginlik.0h@icloud.com>
2026-05-11 11:19:04 +0300

    auto zip building

---
+++ resources/kurulum.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo tmtcopen by İsmail Uygar
+echo Kurulum Başlatılıyor...
+
+echo tmtcopen binarysi kopyalanıyor...
+sudo cp tmtcopen /usr/bin/tmtcopen
+sudo chmod +x /usr/bin/tmtcopen
+
+echo tmtcopen ikonu kopyalanıyor...
+sudo cp logo.png /usr/share/pixmaps/tmtcopen.png
+
+echo .desktop dosyası kopyalanıyor...
+sudo cp tmtcopen.desktop /usr/share/applications/tmtcopen.desktop
+sudo chmod +x /usr/share/applications/tmtcopen.desktop
