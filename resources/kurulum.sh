e5864aee0686a9be2ba44f6d4f8d9ffdc901b3d2
Ismail <solist.cilginlik.0h@icloud.com>
2026-05-11 12:56:23 +0300

    kurulum.sh guncellemesi

--- resources/kurulum.sh
+++ resources/kurulum.sh
@@ -4,7 +4,7 @@ echo tmtcopen by İsmail Uygar
 echo Kurulum Başlatılıyor...
 
 echo maim indiriliyor
-sudo apt install maim -y
+sudo apt update && sudo apt install -y libgtk-3-0 libgdk-pixbuf2.0-0 libcairo2 libglib2.0-0 libpango-1.0-0 libatk1.0-0 maim
 
 echo tmtcopen binarysi kopyalanıyor...
 sudo cp tmtcopen /usr/bin/tmtcopen
